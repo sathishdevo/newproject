596c28d0b185b4dfd117008a37075c51e718f00d
sathishdevo <sathishveerabathiran11@gmail.com>
2023-05-13 15:46:04 +0000

    3rdcommit

--- BB.java
+++ BB.java
@@ -1,12 +1,6 @@
 sample code
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Java");  
+     System.out.println("Hello all");  
     }  
-} 
---------------------------------------------
-
-Git
-launch linux instance & install gitrepository using below comments
- yum install git -y
-git --version
+}
