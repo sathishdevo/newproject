0fef9dc3acf2b2c50131d1941ccecb50d58a3105
root <root@ip-172-31-45-131.ap-south-1.compute.internal>
2023-05-13 14:29:38 +0000

    1stcommit

---
+++ BB.java
@@ -0,0 +1,12 @@
+sample code
+class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello Java");  
+    }  
+} 
+--------------------------------------------
+
+Git
+launch linux instance & install gitrepository using below comments
+ yum install git -y
+git --version
